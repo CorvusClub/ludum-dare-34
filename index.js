748345c1e2c928ece98da21c58dee68a7b2fd18a
Tom Shea <thristhart@gmail.com>
2015-12-14 06:27:27 +0000

    loop audio

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ class Game {
         
         this.song = document.createElement("audio");
         this.song.src = "./rain_nice_today.mp3";
+        this.song.loop = true;
         this.song.play();
         document.querySelector("#music").addEventListener("click", () => {
             if(this.song.paused) {
