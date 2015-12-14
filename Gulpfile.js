557eb85a62c19f05089b7405d583c2001d4cc2db
Tom Shea <thristhart@gmail.com>
2015-12-14 02:43:38 +0000

    Two day's progress

--- Gulpfile.js
+++ Gulpfile.js
@@ -6,9 +6,14 @@ const buffer = require('vinyl-buffer');
 const browserify = require('browserify');
 const babel = require('babelify');
 
+const sourcemaps = require("gulp-sourcemaps");
+
 const postcss = require('gulp-postcss');
 const autoprefixer = require('autoprefixer');
 const importCSS = require('postcss-import');
+
+const svgstore = require("gulp-svgstore");
+const inject = require("gulp-inject")
 
 const browserSync = require('browser-sync');
 
@@ -26,7 +31,7 @@ gulp.task('javascript', () => {
             console.error(err);
             this.emit('end')
         })
-        .pipe(source('index.js'))
+        .pipe(source('app.js'))
         .pipe(buffer())
         .pipe(gulp.dest('./build'));
 });
@@ -37,7 +42,9 @@ function css() {
     ];
     
     return gulp.src("index.css")
+        .pipe(sourcemaps.init())
         .pipe(postcss(processors))
+        .pipe(sourcemaps.write())
         .on('error', function(err) {
           console.error(err);
           this.emit('end');
@@ -53,16 +60,38 @@ gulp.task('browserSync-js', ['javascript'], () => {
     browserSync.reload();
 });
 
-gulp.task('serve', () => {
+gulp.task('svgstore', () => {
+    var svgs = gulp.src("./assets/*.svg")
+        .pipe(svgstore({inlineSvg: true}));
+        
+    return gulp.src("./index.html")
+        .pipe(inject(svgs, {transform: (filePath, file) => {
+            return file.contents.toString();
+        }}))
+        .pipe(gulp.dest("./build/"));
+});
+
+gulp.task('assets', () => {
+    gulp.src("./assets/*")
+        .pipe(gulp.dest("./build/assets/"));
+})
+
+gulp.task('reinject-svg', ['svgstore'], browserSync.reload)
+
+gulp.task('default', ['svgstore', 'javascript', 'css', 'assets']);
+
+gulp.task('serve', ['default'], () => {
     browserSync.init({
         server: {
-            baseDir: "./"
+            baseDir: "./build"
         },
+        ghostMode: false,
         port: 8082
     });
     gulp.watch("./index.js", ['browserSync-js']);
     gulp.watch("./lib/*.js", ['browserSync-js']);
     gulp.watch("./index.css", ['synccss']);
     gulp.watch("./style/*.css", ['synccss']);
-    gulp.watch("./index.html", browserSync.reload)
+    gulp.watch("./index.html", ['reinject-svg']);
+    gulp.watch("./assets/*", ['assets']);
 })
--- index.js
+++ index.js
@@ -4,16 +4,30 @@ import Engine from "./lib/engine.js";
 class Game {
     constructor(canvas) {
         this.engine = new Engine();
-        this.renderer = new Renderer(canvas, this.engine, 412, 660);
+        this.renderer = new Renderer(canvas, this.engine, 1000, 1460.5);
         
         this.lastTick = performance.now();
         
-        this.update();
+        this.update(this.lastTick);
+        
+        this.song = document.createElement("audio");
+        this.song.src = "./rain_nice_today.mp3";
+        //this.song.play();
+        document.querySelector("#music").addEventListener("click", () => {
+            if(this.song.paused) {
+                this.song.play();
+            }
+            else {
+                this.song.pause();
+            }
+        })
     }
     
     update(currentTime) {
         let tick = currentTime - this.lastTick;
-        
+        if(tick > 128) {
+            tick = 128;
+        }
         this.engine.update(tick, currentTime);
         this.renderer.update(tick, currentTime);
         
@@ -23,4 +37,4 @@ class Game {
     }
 }
 
-var game = new Game(document.getElementById("canvas"));
+window.game = new Game(document.getElementById("canvas"));
